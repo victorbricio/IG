3cffa6f75ebf204b75075bf266267dbb1e59579f
victorbricio <zbricioblazquez@gmail.com>
2021-07-23 10:14:29 +0200

    Delete srcs-alum directory

--- srcs-alum/Material.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MATERIAL_HPP
-#define MATERIAL_HPP
-
-
-class Material{
-public:
-  virtual void activar() = 0;
-};
-#endif
--- srcs-alum/Objeto3D.cpp
+++
@@ -1,5 +0,0 @@
-#include "Objeto3D.hpp"
-
-std::string Objeto3D::nombre() {
-  return nombre_obj;
-}
